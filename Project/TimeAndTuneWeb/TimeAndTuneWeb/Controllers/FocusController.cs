589ca0b854f3fe3ffcc0757507e05cb669d296cd
Nazarii Huliak <96128629+NazariiHuliak@users.noreply.github.com>
2024-04-26 01:19:40 +0300

    Created focus page

---
+++ Project/TimeAndTuneWeb/TimeAndTuneWeb/Controllers/FocusController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TimeAndTuneWeb.Controllers
+{
+    public class FocusController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
