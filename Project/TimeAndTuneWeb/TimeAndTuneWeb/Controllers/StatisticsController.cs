ddd420da825976c551f7dd83f5a4daa9a1e6b2b8
AndriyK12 <124818943+AndriyK12@users.noreply.github.com>
2024-05-10 00:07:04 +0300

    little fixes

--- Project/TimeAndTuneWeb/TimeAndTuneWeb/Controllers/StatisticsController.cs
+++ Project/TimeAndTuneWeb/TimeAndTuneWeb/Controllers/StatisticsController.cs
@@ -6,17 +6,9 @@
     public class StatisticsController : Controller
     {
 
-        public int overallWeekTasksAmount;
-        public int overallMonthTasksAmount;
-        public int overallYearTasksAmount;
-        public int completedWeekTasksAmount;
-        public int completedMonthTasksAmount;
-        public int completedYearTasksAmount;
-
         public IActionResult Statistics()
         {
             // Week chart
-
             DateOnly currentDate = DateOnly.FromDateTime(DateTime.Now);
             int currentDayOfWeek = (int)currentDate.DayOfWeek;
             int daysToMonday = currentDayOfWeek - (int)DayOfWeek.Monday;
@@ -32,11 +24,8 @@
             DatabaseUserProvider userService = new DatabaseUserProvider();
             foreach (DateOnly date in dates)
             {
-                int overallAmount = taskService.GetAmountOfTasksByDate(date, 1/*userService.getUserID(MainWindow.ActiveUser)*/);
                 int amount = taskService.GetAmountOfCompletedTasksByDate(date, 1/*userService.getUserID(MainWindow.ActiveUser)*/);
                 tasks.Add(amount);
-                completedWeekTasksAmount += amount;
-                overallWeekTasksAmount += overallAmount;
             }
 
             var weekLabels = dates.Select(date => date.ToString("dd/MM")).ToArray();
@@ -47,7 +36,6 @@
 
 
             // Month chart
-
             DateOnly firstDayOfMonth = new DateOnly(currentDate.Year, currentDate.Month, 1);
             DateOnly lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddDays(-1);
             int daysInMonth = (int)lastDayOfMonth.Day - firstDayOfMonth.Day + 1;
@@ -60,11 +48,8 @@
             tasks = new List<int>();
             foreach (DateOnly date in dates)
             {
-                int overallAmount = taskService.GetAmountOfTasksByDate(date, 1/*userService.getUserID(MainWindow.ActiveUser)*/);
                 int amount = taskService.GetAmountOfCompletedTasksByDate(date, 1/*userService.getUserID(MainWindow.ActiveUser)*/);
                 tasks.Add(amount);
-                completedMonthTasksAmount += amount;
-                overallMonthTasksAmount += overallAmount;
             }
 
             var monthLabels = dates.Select(date => date.ToString("dd/MM")).ToArray();
@@ -74,7 +59,6 @@
             ViewBag.MonthCompletedTasks = monthData;
 
             // Year chart
-
             int currentYear = currentDate.Year;
             Dictionary<int, List<DateOnly>> dateDictionary = new Dictionary<int, List<DateOnly>>();
             for (int month = 1; month <= 12; month++)
@@ -93,23 +77,19 @@
             tasks = new List<int>();
             foreach (var kvp in dateDictionary)
             {
-                Console.WriteLine($"Місяць {kvp.Key}:");
                 int amount_of_tasks_by_month = 0;
-                int overall_amount = 0;
                 foreach (var date in kvp.Value)
                 {
-                    overall_amount += taskService.GetAmountOfTasksByDate(date, 1/*userService.getUserID(MainWindow.ActiveUser)*/);
                     amount_of_tasks_by_month += taskService.GetAmountOfCompletedTasksByDate(date, 1/*userService.getUserID(MainWindow.ActiveUser)*/);
                 }
+
                 tasks.Add(amount_of_tasks_by_month);
-                completedYearTasksAmount += amount_of_tasks_by_month;
-                overallYearTasksAmount += overall_amount;
             }
 
             var monthNames = new string[]
             {
                     "January", "February", "March", "April", "May", "June",
-                    "July", "August", "September", "October", "November", "December"
+                    "July", "August", "September", "October", "November", "December",
             };
 
             var yearLabels = monthNames;
